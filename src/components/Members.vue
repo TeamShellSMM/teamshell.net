fe3330b711062783285d5ae334af799f3de8f536
a-unique-id <45527149+a-unique-id@users.noreply.github.com>
2020-04-07 21:13:59 +0800

    Added voting amounts for normal list. And shellder comments

--- src/components/Members.vue
+++ src/components/Members.vue
@@ -293,7 +293,7 @@
 
           let that = this;
 
-          loadTeamshellApi(function(_rawData,dataNoChange){
+          loadTeamshellApi(that.$store.state.token,function(_rawData,dataNoChange){
             if(dataNoChange){
               $.notify("No new data was loaded",{
                 className:"success",
