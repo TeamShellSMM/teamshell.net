ae40170fc96d02b613e154e1edb9d1b92ac60a23
Liaf <liafdev@gmail.com>
2020-05-28 13:52:18 +0200

    fix

--- src/services/helper-service.js
+++ src/services/helper-service.js
@@ -29,7 +29,7 @@ let loadEndpoint = function({type='post',route='json',that,onLoad,data={}}){
       } else {
         console.log(_data)
         that.$store.commit(that.$route.params.team + '/setTeamAdmin', _data.teamAdmin);
-        if(data.competitions){
+        if(_data.competitions){
           that.$store.commit(that.$route.params.team + '/setTeamCompetitions', _data.competitions);
           that.$store.commit(that.$route.params.team + '/setTeamLastCompWinner', _data);
         }
