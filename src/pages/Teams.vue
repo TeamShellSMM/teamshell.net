6edf713f934e4cad6886066051981e59cce4f729
Liaf <liafdev@gmail.com>
2020-08-29 22:00:53 +0200

    added team powerup

--- src/pages/Teams.vue
+++ src/pages/Teams.vue
@@ -110,6 +110,19 @@
             </div>
           </div>
         </div>
+        <div class="col-md-4 col-sm-6 p-1">
+          <div class="card team-card teampowerup-primary-bc">
+            <img src="/assets/teams/teampowerup/logo.png" class="card-img-top card-img-team-logo card-img-rounded-logo" alt="logo">
+            <div class="card-body">
+              <h2 class="card-title"><router-link to="/teampowerup"><span class="teampowerup-primary-fg">Team</span><span class="teampowerup-secondary-fg"> Power Up</span></router-link></h2>
+              <p class="card-text">
+                Team Power Up makes kaizo levels which are focused around one or more power ups (Yoshi, shellmets, goomba shoes and hats excluded).
+              </p>
+              <p class="card-text">Their motto right now is: "Feeling Powerful", so if that gets you powered up, check them out.</p>
+              <router-link to="/teampowerup" class="btn teampowerup-primary-bg team-card-nav-button" style="color:white;">Go check it out!</router-link>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
     <div class="row mt-3">
